c55b218d7931f42c45a57c9dc242b8240ed5f444
jimbob88 <blackburnfjames@gmail.com>
2019-03-10 15:08:19 +0000

    Added AutoUpdating Script

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+from __future__ import absolute_import
 import sys
+from io import open
 if sys.version_info >= (3, 0):
     import beer_engine
+    from urllib.request import urlopen
 else:
     import beer_engine2 as beer_engine
+    from urllib2 import urlopen
 
-beer_engine.main()
+
+if __name__ == u'__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == u'--update':
+            with open(u'update.py', u'r') as f:
+                exec(f.read())
+            update()
+        elif sys.argv[1] == u'--coreupdate':
+            with urlopen(u'https://raw.githubusercontent.com/jimbob88/wheelers-wort-works/master/update.py') as response:
+                exec(response.read().decode(u'utf-8'))
+            update()
+        else:
+            print(u'Run --update to update the current install, or run --coreupdate to update the updater script')
+            exit()
+    beer_engine.main()
---
+++ update.py
@@ -0,0 +1,24 @@
+def update():
+    files = {
+        'beer_engine.py': 'https://raw.githubusercontent.com/jimbob88/wheelers-wort-works/master/beer_engine.py',
+        'beer_engine2.py': 'https://raw.githubusercontent.com/jimbob88/wheelers-wort-works/master/beer_engine2.py',
+        'brew_data.py': 'https://raw.githubusercontent.com/jimbob88/wheelers-wort-works/master/brew_data.py',
+        'brew_data2.py': 'https://raw.githubusercontent.com/jimbob88/wheelers-wort-works/master/brew_data2.py'
+    }
+
+    for file, url in files.items():
+        print('Updating {file} from {url}'.format(file=file, url=url))
+        if sys.version_info >= (3, 0):
+            with urlopen(url) as response, open(file, 'wb') as out_file:
+                data = response.read()
+                out_file.write(data)
+        else:
+            response = urlopen(url)
+            out_file = open(file, 'wb')
+            data = response.read()
+            out_file.write(data)
+
+    if sys.version_info >= (3, 0):
+        import beer_engine
+    else:
+        import beer_engine2 as beer_engine
